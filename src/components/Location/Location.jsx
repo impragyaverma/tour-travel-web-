0ffacbdc6c4830b752f9a15e5202cc7ce7fc2ab4
impragyaverma <pragya9428@gmail.com>
2024-08-01 20:10:46 +0530

    Update Location.jsx

--- src/components/Location/Location.jsx
+++ src/components/Location/Location.jsx
@@ -12,7 +12,7 @@ const Location = () => {
 
           <div className="rounded-xl ">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7116.040113933064!2d83.97168138953894!3d26.90285855661167!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39938d631905f7c1%3A0x189dbadf0b42da46!2sNew%20Mobile%20World!5e0!3m2!1sen!2sin!4v1700902186385!5m2!1sen!2sin"
+              src="https:"<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d56985.18408335603!2d82.15796777247463!3d26.789845860732477!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x399a07937e6d2823%3A0x5fc8f683b17f222b!2sAyodhya%2C%20Uttar%20Pradesh!5e0!3m2!1sen!2sin!4v1722523229521!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>"
               width="100%"
               height="360"
               allowfullscreen=""
